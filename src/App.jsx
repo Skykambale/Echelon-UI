3637553d8d3f68c6a3a9d3f973816dc32446ea43
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2025-02-16 00:27:58 +0530

    Add clerk auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import TaskDashboard from "./app/pages/tasks/tasks-dashboard";
 import RootLayout from "./app/pages/root-container/RootLayout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NewTask from "./app/components/NewTask";
+import { SignedOut, SignInButton } from "@clerk/clerk-react";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path="/home"
+          element={
+            <SignedOut>
+              <div>
+                {" "}
+                Login page <SignInButton />
+              </div>
+            </SignedOut>
+          }
+        ></Route>
+
         <Route path="/" element={<RootLayout />}>
           <Route index element={<TaskDashboard />}></Route>
           <Route path="new-task" element={<NewTask />}></Route>
--- src/app/pages/root-container/RootLayout.jsx
+++ src/app/pages/root-container/RootLayout.jsx
@@ -1,24 +1,28 @@
 import "./root-layout.css";
 import Navbar from "../../components/Navbar";
 import { Outlet } from "react-router-dom";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 const RootLayout = () => {
   return (
-    <div className="root-container bg-zinc-950 text-zinc-300">
-      <header className="root-header h-[60px]">
-        <h1 className="flex items-center py-2 px-4 text-3xl text-zinc-300">
-          Lumina
-        </h1>
-      </header>
-      <div className="root-content flex">
-        <aside className="main-side-panel">
-          <Navbar />
-        </aside>
-        <main className="main-content-container">
-          <Outlet />
-        </main>
+    <SignedIn>
+      <div className="root-container bg-zinc-950 text-zinc-300">
+        <header className="root-header h-[60px] flex justify-between items-center px-3">
+          <h1 className="flex items-center py-2 px-4 text-3xl text-zinc-300">
+            Echelon
+          </h1>
+          <UserButton />
+        </header>
+        <div className="root-content flex">
+          <aside className="main-side-panel">
+            <Navbar />
+          </aside>
+          <main className="main-content-container">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </SignedIn>
   );
 };
 
