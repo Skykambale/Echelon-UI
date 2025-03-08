5ad7a42b1cbc55b22d31664cb720028afe43b373
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2025-03-08 23:40:11 +0530

    Add icons in navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { BsStars } from "react-icons/bs";
+import { FaChartSimple } from "react-icons/fa6";
+import { FaTasks } from "react-icons/fa";
 
 const navOptions = [
-  { label: "Visualize", path: "/visualize" },
-  { label: "Tasks", path: "/" },
-  { label: "AI", path: "/ai" },
+  { label: "Visualize", path: "/visualize", icon: <FaChartSimple size={24} /> },
+  { label: "Tasks", path: "/", icon: <FaTasks /> },
+  { label: "AI", path: "/ai", icon: <BsStars /> },
 ];
 
 const Navbar = () => {
   const [selectedTab, setSelectedTab] = useState("/");
   const navigate = useNavigate();
+  const showTabLabel = true;
 
   const handleNavLinkClick = (tab) => {
     setSelectedTab(tab);
@@ -17,16 +21,17 @@ const Navbar = () => {
   };
   return (
     <div className="w-full h-full flex flex-col justify-center px-3">
-      <ul className="text-2xl text-center text-zinc-500">
+      <ul className="text-2xl  text-zinc-500">
         {navOptions.map((link) => (
           <li
-            className={`my-4 py-1 cursor-pointer w-full ${
-              selectedTab === link.path ? "text-zinc-300 text-3xl" : ""
+            className={`my-4 py-1 cursor-pointer w-full flex items-center gap-3 ${
+              selectedTab === link.path ? "text-zinc-300" : ""
             }`}
             key={link.path}
             onClick={() => handleNavLinkClick(link.path)}
           >
-            {link.label}
+            <p>{link.icon}</p>
+            {showTabLabel && <p>{link.label}</p>}
           </li>
         ))}
       </ul>
