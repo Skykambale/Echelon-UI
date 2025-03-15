51cecaff7b5fd9b9bdc6d21548ed21adb8fa9f52
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2025-03-15 14:39:03 +0530

    Refactor UI

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BsStars } from "react-icons/bs";
 import { FaChartSimple } from "react-icons/fa6";
 import { FaTasks } from "react-icons/fa";
+import { UserButton } from "@clerk/clerk-react";
 
 const navOptions = [
   { label: "Visualize", path: "/visualize", icon: <FaChartSimple size={24} /> },
@@ -13,28 +14,48 @@ const navOptions = [
 const Navbar = () => {
   const [selectedTab, setSelectedTab] = useState("/");
   const navigate = useNavigate();
-  const showTabLabel = true;
+  const [showTabLabel, setShowTabLabel] = useState(true);
 
   const handleNavLinkClick = (tab) => {
     setSelectedTab(tab);
     navigate(tab);
   };
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      console.log("Mobile device detected");
+      setShowTabLabel(false);
+    }
+  }, []);
   return (
-    <div className="w-full h-full flex flex-col justify-center px-3">
-      <ul className="text-2xl  text-zinc-500">
-        {navOptions.map((link) => (
-          <li
-            className={`my-4 py-1 cursor-pointer w-full flex items-center gap-3 ${
-              selectedTab === link.path ? "text-zinc-300" : ""
-            }`}
-            key={link.path}
-            onClick={() => handleNavLinkClick(link.path)}
-          >
-            <p>{link.icon}</p>
-            {showTabLabel && <p>{link.label}</p>}
-          </li>
-        ))}
-      </ul>
+    <div className="h-full flex flex-col justify-between py-2">
+      {/* Header */}
+      <h1 className="flex items-center py-2 px-4 text-3xl text-zinc-300">
+        Echelon
+      </h1>
+
+      {/* Nav links */}
+      <div className="w-full h-full flex flex-col justify-center px-3">
+        <ul className="text-xl  text-zinc-500">
+          {navOptions.map((link) => (
+            <li
+              className={`my-4 py-1 cursor-pointer w-full flex items-center gap-3 ${
+                selectedTab === link.path ? "text-zinc-300" : ""
+              }`}
+              key={link.path}
+              onClick={() => handleNavLinkClick(link.path)}
+            >
+              <p>{link.icon}</p>
+              {showTabLabel && <p>{link.label}</p>}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* User button */}
+      <div className="pl-4 p-2 ">
+        <UserButton />
+      </div>
     </div>
   );
 };
--- src/app/pages/root-container/RootLayout.jsx
+++ src/app/pages/root-container/RootLayout.jsx
@@ -1,7 +1,7 @@
 import "./root-layout.css";
 import Navbar from "../../components/Navbar";
 import { Outlet } from "react-router-dom";
-import { SignedIn, UserButton, useUser } from "@clerk/clerk-react";
+import { SignedIn, useUser } from "@clerk/clerk-react";
 
 const RootLayout = () => {
   const user = useUser();
@@ -9,16 +9,11 @@ const RootLayout = () => {
   return (
     <SignedIn>
       <div className="root-container bg-zinc-950 text-zinc-300">
-        <header className="root-header h-[60px] flex justify-between items-center px-3">
-          <h1 className="flex items-center py-2 px-4 text-3xl text-zinc-300">
-            Echelon
-          </h1>
-          <UserButton />
-        </header>
         <div className="root-content flex">
           <aside className="main-side-panel">
             <Navbar />
           </aside>
+
           <main className="main-content-container">
             <Outlet />
           </main>
