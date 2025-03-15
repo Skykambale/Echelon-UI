8802ac47636ad123a64b2ed466ed4ed5cd3d65f0
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2025-03-15 15:26:36 +0530

    Change nav styles

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BsStars } from "react-icons/bs";
 import { FaChartSimple } from "react-icons/fa6";
 import { FaTasks } from "react-icons/fa";
 import { UserButton } from "@clerk/clerk-react";
+import PropTypes from "prop-types";
 
 const navOptions = [
   { label: "Visualize", path: "/visualize", icon: <FaChartSimple size={24} /> },
@@ -11,31 +12,28 @@ const navOptions = [
   { label: "AI", path: "/ai", icon: <BsStars /> },
 ];
 
-const Navbar = () => {
+const Navbar = ({ isMobileView }) => {
   const [selectedTab, setSelectedTab] = useState("/");
   const navigate = useNavigate();
-  const [showTabLabel, setShowTabLabel] = useState(true);
 
   const handleNavLinkClick = (tab) => {
     setSelectedTab(tab);
     navigate(tab);
   };
 
-  useEffect(() => {
-    if (window.innerWidth <= 768) {
-      console.log("Mobile device detected");
-      setShowTabLabel(false);
-    }
-  }, []);
   return (
-    <div className="h-full flex flex-col justify-between py-2">
+    <div className="h-full flex flex-col justify-between py-4">
       {/* Header */}
-      <h1 className="flex items-center py-2 px-4 text-3xl text-zinc-300">
-        Echelon
+
+      <h1
+        className="flex items-center py-2 px-1 text-3xl text-zinc-300 cursor-pointer font-cursive"
+        onClick={() => navigate("/")}
+      >
+        {isMobileView ? "E" : "Echelon"}
       </h1>
 
       {/* Nav links */}
-      <div className="w-full h-full flex flex-col justify-center px-3">
+      <div className="w-full h-full flex flex-col justify-center  px-1">
         <ul className="text-xl  text-zinc-500">
           {navOptions.map((link) => (
             <li
@@ -45,19 +43,23 @@ const Navbar = () => {
               key={link.path}
               onClick={() => handleNavLinkClick(link.path)}
             >
-              <p>{link.icon}</p>
-              {showTabLabel && <p>{link.label}</p>}
+              <p title={link.label}>{link.icon}</p>
+              {!isMobileView && <p>{link.label}</p>}
             </li>
           ))}
         </ul>
       </div>
 
       {/* User button */}
-      <div className="pl-4 p-2 ">
+      <div className="px-1 ">
         <UserButton />
       </div>
     </div>
   );
+};
+
+Navbar.propTypes = {
+  isMobileView: PropTypes.bool.isRequired,
 };
 
 export default Navbar;
--- src/app/pages/root-container/RootLayout.jsx
+++ src/app/pages/root-container/RootLayout.jsx
@@ -2,19 +2,37 @@ import "./root-layout.css";
 import Navbar from "../../components/Navbar";
 import { Outlet } from "react-router-dom";
 import { SignedIn, useUser } from "@clerk/clerk-react";
+import { useEffect, useState } from "react";
 
 const RootLayout = () => {
   const user = useUser();
   console.log(user);
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 768) {
+      console.log("Mobile device detected");
+      setIsMobileView(true);
+    }
+  }, []);
+
   return (
     <SignedIn>
       <div className="root-container bg-zinc-950 text-zinc-300">
         <div className="root-content flex">
-          <aside className="main-side-panel">
-            <Navbar />
+          <aside
+            className={`main-side-panel ${
+              isMobileView ? "w-[12%]" : "w-[145px]"
+            }`}
+          >
+            <Navbar isMobileView={isMobileView} />
           </aside>
 
-          <main className="main-content-container">
+          <main
+            className={`main-content-container ${
+              isMobileView ? "w-[86%]" : "w-full"
+            }`}
+          >
             <Outlet />
           </main>
         </div>
