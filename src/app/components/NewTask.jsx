069eb42c6d89968b772b8c5c018191616c7b4a61
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2025-04-02 01:39:48 +0530

    Add security client

--- src/app/components/NewTask.jsx
+++ src/app/components/NewTask.jsx
@@ -1,132 +1,125 @@
 import { Button } from "@/components/ui/button";
 import { DatePicker } from "@/components/ui/date-picker";
 import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import PropTypes from "prop-types";
 import { X } from "lucide-react";
+import Loading from "./LoadingSpinner";
 
 // Constants
 const categories = [
-  { label: "Personal", value: "personal" },
-  { label: "Work", value: "work" },
-  { label: "Academics", value: "academics" },
+	{ label: "Personal", value: "personal" },
+	{ label: "Work", value: "work" },
+	{ label: "Academics", value: "academics" },
 ];
 
 const defaultInputValues = {
-  title: "",
-  description: "",
-  category: "",
-  date: new Date(),
+	title: "",
+	description: "",
+	category: "",
+	taskDate: new Date(),
 };
 
 // component
 const NewTask = ({ onClose, onSubmit }) => {
-  const [inputData, setInputData] = useState(defaultInputValues);
+	const [inputData, setInputData] = useState(defaultInputValues);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const handleInputChange = (field, value) => {
-    setInputData({ ...inputData, [field]: value });
-    console.log(field, value);
-  };
+	const handleInputChange = (field, value) => {
+		setInputData({ ...inputData, [field]: value });
+		console.log(field, value);
+	};
 
-  const handleOnCreate = () => {
-    console.log("API CALLL :: ", inputData);
-    onSubmit();
-  };
+	const handleOnCreate = async () => {
+		console.log("API CALLL :: ", inputData);
+		setIsLoading(true);
+		await onSubmit(inputData);
+		setTimeout(() => setIsLoading(false), 3000);
+	};
 
-  const handleCloseModal = () => {
-    setInputData(defaultInputValues);
-    onClose();
-  };
+	const handleCloseModal = () => {
+		setInputData(defaultInputValues);
+		onClose();
+	};
 
-  return (
-    <div className="w-full flex flex-col gap-4 lg:w-1/2">
-      <div className="border border-slate-600 rounded-md bg-[#111]">
-        <div>
-          <div className="p-2 flex justify-between items-center lg:p-4">
-            <h3>Create a New Task</h3>
+	return (
+		<div className="w-full flex flex-col gap-4 lg:w-1/2">
+			{isLoading && <Loading />}
+			<div className="border border-slate-600 rounded-md bg-[#111]">
+				<div>
+					<div className="p-2 flex justify-between items-center lg:p-4">
+						<h3>Create a New Task</h3>
 
-            <X onClick={handleCloseModal} className="cursor-pointer" />
-          </div>
-          <div className="w-full h-[.5px] bg-slate-600"></div>
+						<X onClick={handleCloseModal} className="cursor-pointer" />
+					</div>
+					<div className="w-full h-[.5px] bg-slate-600"></div>
 
-          <div className="p-2 h-[60vh] overflow-auto flex flex-col gap-2 lg:p-4 lg:gap-4">
-            {/* Inputs */}
-            <Input
-              className={"bg-[#222] py-6 "}
-              placeholder={"Title *"}
-              onChange={(e) => handleInputChange("title", e.target.value)}
-              value={inputData.title}
-            />
-            <Textarea
-              className={"bg-[#222]"}
-              placeholder={"Description (optional)"}
-              rows={16}
-              value={inputData.description}
-              onChange={(e) => handleInputChange("description", e.target.value)}
-            />
+					<div className="p-2 h-[60vh] overflow-auto flex flex-col gap-2 lg:p-4 lg:gap-4">
+						{/* Inputs */}
+						<Input
+							className={"bg-[#222] py-6 "}
+							placeholder={"Title *"}
+							onChange={(e) => handleInputChange("title", e.target.value)}
+							value={inputData.title}
+						/>
+						<Textarea
+							className={"bg-[#222]"}
+							placeholder={"Description (optional)"}
+							rows={16}
+							value={inputData.description}
+							onChange={(e) => handleInputChange("description", e.target.value)}
+						/>
 
-            <div className="flex justify-between gap-4 w-full">
-              <Select
-                onValueChange={(value) => handleInputChange("category", value)}
-                value={inputData.category}
-                defaultValue="work"
-              >
-                <SelectTrigger className={`w-full bg-[#222] py-6 `}>
-                  <SelectValue placeholder="Work" />
-                </SelectTrigger>
-                <SelectContent>
-                  {categories.map((item) => (
-                    <SelectItem value={item.value} key={item.value}>
-                      {item.label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+						<div className="flex justify-between gap-4 w-full">
+							<Select
+								onValueChange={(value) => handleInputChange("category", value)}
+								value={inputData.category}
+								defaultValue="work"
+							>
+								<SelectTrigger className={`w-full bg-[#222] py-6 `}>
+									<SelectValue placeholder="Work" />
+								</SelectTrigger>
+								<SelectContent>
+									{categories.map((item) => (
+										<SelectItem value={item.value} key={item.value}>
+											{item.label}
+										</SelectItem>
+									))}
+								</SelectContent>
+							</Select>
 
-              <DatePicker
-                className={
-                  "w-full py-6  justify-start text-left font-normal text-zinc-700 bg-[#222]"
-                }
-                date={inputData.date}
-                setDate={(date) => handleInputChange("date", date)}
-              />
-            </div>
-          </div>
+							<DatePicker
+								className={
+									"w-full py-6  justify-start text-left font-normal text-zinc-700 bg-[#222]"
+								}
+								date={inputData.taskDate}
+								setDate={(date) => handleInputChange("taskDate", date)}
+							/>
+						</div>
+					</div>
 
-          <div className="w-full h-[.5px] bg-slate-600"></div>
+					<div className="w-full h-[.5px] bg-slate-600"></div>
 
-          <div className="p-2 flex justify-end items-center gap-3">
-            <Button
-              className={"bg-red-800 hover:bg-red-900"}
-              onClick={handleCloseModal}
-            >
-              Cancel
-            </Button>
-            <Button
-              className={"bg-green-800 hover:bg-green-900"}
-              onClick={handleOnCreate}
-            >
-              Create
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+					<div className="p-2 flex justify-end items-center gap-3">
+						<Button className={"bg-red-800 hover:bg-red-900"} onClick={handleCloseModal}>
+							Cancel
+						</Button>
+						<Button className={"bg-green-800 hover:bg-green-900"} onClick={handleOnCreate}>
+							Create
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 // Proptypes
 NewTask.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+	onClose: PropTypes.func.isRequired,
+	onSubmit: PropTypes.func.isRequired,
 };
 
 export default NewTask;
--- src/app/pages/tasks/tasks-dashboard.jsx
+++ src/app/pages/tasks/tasks-dashboard.jsx
@@ -1,156 +1,156 @@
 import { DatePicker } from "@/components/ui/date-picker";
-import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { useEffect, useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import Task from "@/app/components/Task";
 import NewTask from "@/app/components/NewTask";
+import { useRestSecurityClient } from "@/app/hooks/securityClient";
+import Loading from "@/app/components/LoadingSpinner";
 
 const TaskDashboard = () => {
-  const [date, setDate] = useState(new Date());
-  const [selectedFilters, setSelectedFilters] = useState({});
-  const [showNewTask, setShowNewTask] = useState(false);
+	const [date, setDate] = useState(new Date());
+	const [selectedFilters, setSelectedFilters] = useState({});
+	const [showNewTask, setShowNewTask] = useState(false);
+	const restClient = useRestSecurityClient();
+	const [isLoading, setIsLoading] = useState(false);
 
-  const filters = [
-    { label: "Productive", value: "productive" },
-    { label: "Super Productive", value: "super productive" },
-  ];
+	const filters = [
+		{ label: "Productive", value: "productive" },
+		{ label: "Super Productive", value: "super productive" },
+	];
 
-  const handleDropdownChange = (dropdown, newValue) => {
-    setSelectedFilters({ ...selectedFilters, [dropdown]: newValue });
-  };
-  const handleChangeDateByOne = (date, type) => {
-    if (type === "next") {
-      setDate(new Date(date.setDate(date.getDate() + 1)));
-    } else if (type === "previous") {
-      setDate(new Date(date.setDate(date.getDate() - 1)));
-    }
-  };
-  return (
-    <div className="w-100 h-full flex items-center justify-center p-2">
-      {showNewTask ? (
-        <NewTask onClose={() => setShowNewTask(false)} />
-      ) : (
-        <div className="w-full flex flex-col gap-4 lg:w-1/2">
-          <div className="flex justify-center items-center p-2 gap-4 ">
-            <Button
-              size="icon"
-              onClick={() => handleChangeDateByOne(date, "previous")}
-            >
-              <ChevronLeft />
-            </Button>
-            <DatePicker date={date} setDate={setDate} />
-            <Button
-              size="icon"
-              onClick={() => handleChangeDateByOne(date, "next")}
-            >
-              <ChevronRight />
-            </Button>
-          </div>
-          <div className="border border-slate-600 rounded bg-[#111]">
-            <div>
-              <div className="p-2 flex justify-between items-center">
-                <Select
-                  onValueChange={(value) =>
-                    handleDropdownChange("filter", value)
-                  }
-                >
-                  <SelectTrigger className="w-[150px]">
-                    <SelectValue placeholder="Filters" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {filters.map((filter) => (
-                      <SelectItem value={filter.value} key={filter.value}>
-                        {filter.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+	const handleDropdownChange = (dropdown, newValue) => {
+		setSelectedFilters({ ...selectedFilters, [dropdown]: newValue });
+	};
+	const handleChangeDateByOne = (date, type) => {
+		if (type === "next") {
+			setDate(new Date(date.setDate(date.getDate() + 1)));
+		} else if (type === "previous") {
+			setDate(new Date(date.setDate(date.getDate() - 1)));
+		}
+	};
 
-                <Select
-                  onValueChange={(value) =>
-                    handleDropdownChange("statusOfDay", value)
-                  }
-                >
-                  <SelectTrigger className="w-[150px]">
-                    <SelectValue placeholder="Status of Day" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {filters.map((filter) => (
-                      <SelectItem value={filter.value} key={filter.value}>
-                        {filter.label}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-              </div>
-              <div className="w-full h-[.5px] bg-slate-600"></div>
-              <div className="p-2 h-[60vh] overflow-auto">
-                {/* here will be the list of tasks */}
-                {tasks.map((task) => (
-                  <Task
-                    key={task.title}
-                    title={task.title}
-                    description={task.description}
-                    status={task.status}
-                  />
-                ))}
-              </div>
-              <div className="w-full h-[.5px] bg-slate-600"></div>
-              <div className="p-2 flex justify-end items-center">
-                <Button
-                  className={"bg-sky-600 hover:bg-sky-700"}
-                  onClick={() => setShowNewTask(true)}
-                >
-                  Add new Task
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {/* Add New Task modal */}
-    </div>
-  );
+	// Dumy userid and date : user_12345, 2025-03-31
+	const getTasks = async () => {
+		setIsLoading(true);
+		await restClient.get(`/tasks/?date=2025-03-31&day=""&statusOfDay=""&userId=user_12345`);
+		setIsLoading(false);
+	};
+
+	const createNewTask = async (inputData) => {
+		inputData.userId = "user_12345";
+		restClient.post(`/tasks/create`, inputData);
+	};
+	useEffect(() => {
+		getTasks();
+	}, []);
+	return (
+		<div className="w-100 h-full flex items-center justify-center p-2">
+			{isLoading && <Loading />}
+			{showNewTask ? (
+				<NewTask onClose={() => setShowNewTask(false)} onSubmit={createNewTask} />
+			) : (
+				<div className="w-full flex flex-col gap-4 lg:w-1/2">
+					<div className="flex justify-center items-center p-2 gap-4 ">
+						<Button size="icon" onClick={() => handleChangeDateByOne(date, "previous")}>
+							<ChevronLeft />
+						</Button>
+						<DatePicker date={date} setDate={setDate} />
+						<Button size="icon" onClick={() => handleChangeDateByOne(date, "next")}>
+							<ChevronRight />
+						</Button>
+					</div>
+					<div className="border border-slate-600 rounded bg-[#111]">
+						<div>
+							<div className="p-2 flex justify-between items-center">
+								<Select onValueChange={(value) => handleDropdownChange("filter", value)}>
+									<SelectTrigger className="w-[150px]">
+										<SelectValue placeholder="Filters" />
+									</SelectTrigger>
+									<SelectContent>
+										{filters.map((filter) => (
+											<SelectItem value={filter.value} key={filter.value}>
+												{filter.label}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+
+								<Select onValueChange={(value) => handleDropdownChange("statusOfDay", value)}>
+									<SelectTrigger className="w-[150px]">
+										<SelectValue placeholder="Status of Day" />
+									</SelectTrigger>
+									<SelectContent>
+										{filters.map((filter) => (
+											<SelectItem value={filter.value} key={filter.value}>
+												{filter.label}
+											</SelectItem>
+										))}
+									</SelectContent>
+								</Select>
+							</div>
+							<div className="w-full h-[.5px] bg-slate-600"></div>
+							<div className="p-2 h-[60vh] overflow-auto">
+								{/* here will be the list of tasks */}
+								{tasks.map((task) => (
+									<Task
+										key={task.title}
+										title={task.title}
+										description={task.description}
+										status={task.status}
+									/>
+								))}
+							</div>
+							<div className="w-full h-[.5px] bg-slate-600"></div>
+							<div className="p-2 flex justify-end items-center">
+								<Button
+									className={"bg-sky-600 hover:bg-sky-700"}
+									onClick={() => setShowNewTask(true)}
+								>
+									Add new Task
+								</Button>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+			{/* Add New Task modal */}
+		</div>
+	);
 };
 
 export default TaskDashboard;
 
 const tasks = [
-  {
-    title: "Task 1",
-    description: "Description 1",
-    status: "inProgress",
-  },
-  {
-    title: "Task 2",
-    description: "Description 2",
-    status: "done",
-  },
-  {
-    title: "Task 3",
-    description: "Description 3",
-    status: "inProgress",
-  },
-  {
-    title: "Task 4",
-    description: "Description 4",
-    status: "todo",
-  },
-  {
-    title: "Task 5",
-    description: "Description 5",
-    status: "done",
-  },
-  {
-    title: "Task 6",
-    description: "Description 6",
-    status: "inProgress",
-  },
+	{
+		title: "Task 1",
+		description: "Description 1",
+		status: "inProgress",
+	},
+	{
+		title: "Task 2",
+		description: "Description 2",
+		status: "done",
+	},
+	{
+		title: "Task 3",
+		description: "Description 3",
+		status: "inProgress",
+	},
+	{
+		title: "Task 4",
+		description: "Description 4",
+		status: "todo",
+	},
+	{
+		title: "Task 5",
+		description: "Description 5",
+		status: "done",
+	},
+	{
+		title: "Task 6",
+		description: "Description 6",
+		status: "inProgress",
+	},
 ];
